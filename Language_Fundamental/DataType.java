bdfd5d736c9c88a722e2f08457fe27c7344b9fd0
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-05-08 18:43:50 -0700

    learning about data types in DataType.java

---
+++ Language_Fundamental/DataType.java
@@ -0,0 +1,26 @@
+public class DataType {
+  public static void main(String[] args) {
+    // Data Types of the Java
+
+    System.out.println("Data Types in Java Language");
+    int number = 12; // numeric with not contain any decimal
+
+    System.out.println("Integer Value: " + number);
+    
+
+    float num = 23.5f; // numeric with decimal but need f to identify it is floating number
+    
+    double weight = 23.232; // floating number
+    
+    char x = 'a'; // single character
+    
+    String name = "Amman"; // Sequence of characters, word
+
+    int[] arr = {1,2,3,4,5,6}; // list of numeric value
+
+    String[] list_name = {"C", "C++", "Java", "Python"}; // list of sequence of characters
+
+  }
+
+
+}
