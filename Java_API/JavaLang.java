c1f5a94d8878d0d04d16e0ac4ee406c2e25c1746
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-05-09 13:54:01 -0700

    complete the boolean reference type

--- Java_API/JavaLang.java
+++ Java_API/JavaLang.java
@@ -1,5 +1,22 @@
+import java.lang.Boolean;
+
 public class JavaLang {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
+    // Boolean 
+    Boolean paid = false;
+
+    // BooleanValue method
+    System.out.println("Value of paid: " + paid.BooleanValue()); // it simply return paid value; the value is false then it will return false;
+
+    Boolean is_alive = false;
+
+    // compareTo method
+    System.out.println("Compare: " + paid.compareTo(is_alive)); // both are false or true it will return 0; vise verse return 1
+
+    // equals method
+    System.out.println("Equality: " + paid.equals(true)); // if both are true then return true;
+
+
+
   }
 }
