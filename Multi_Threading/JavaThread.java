a7f0da9ceae923ddf22d4cd0837aa9042450b464
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-05-11 12:18:31 -0700

    active thread and setname thread

--- Multi_Threading/JavaThread.java
+++ Multi_Threading/JavaThread.java
@@ -1,8 +1,22 @@
 public class JavaThread extends Thread {
   public static void main(String[] args) {
+    // Check the current Thread running
     System.out.println("Thread: " + Thread.currentThread().getName());
+
+    // Check how many Thread are running
+    System.out.println("Active Thread: " + Thread.activeCount());
+
+    // Create a new Thread
     Thread thread1 = new Thread();
-    thread1.setName("Thread-1");
-    System.out.println("Thread-1: " + thread1.getName());
+
+    thread1.setName("T-1");
+
+    System.out.println("Thread Name: " + thread1.getName());
+    // start the thread
+    thread1.start();
+
+    // Check now how many thread is running
+    System.out.println("Active Thread: " + Thread.activeCount()); // 2
+
   }
 }
