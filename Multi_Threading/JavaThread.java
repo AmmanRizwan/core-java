2aea7714b3715dae65fa151aca3a3ec91011274e
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-05-11 12:11:08 -0700

    Threading on java

--- Multi_Threading/JavaThread.java
+++ Multi_Threading/JavaThread.java
@@ -1,5 +1,8 @@
-public class JavaThread {
+public class JavaThread extends Thread {
   public static void main(String[] args) {
-    System.out.println("Thread in Java");
+    System.out.println("Thread: " + Thread.currentThread().getName());
+    Thread thread1 = new Thread();
+    thread1.setName("Thread-1");
+    System.out.println("Thread-1: " + thread1.getName());
   }
 }
