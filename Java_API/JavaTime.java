f2bf80642ea6ce593bcc35dfb9dfd221531489e9
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-05-10 19:59:37 -0700

    java.time module with LocalDate

--- Java_API/JavaTime.java
+++ Java_API/JavaTime.java
@@ -1,5 +1,16 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class JavaTime {
   public static void main(String[] args) {
-    
+    LocalDate today = LocalDate.now();
+    LocalDate tomorrow = today.plusDays(1);
+    LocalDate newMonth = today.plusMonths(1);
+    System.out.println(tomorrow);
+    System.out.println(newMonth);
+
+    LocalTime now = LocalTime.now();
+    System.out.println(now);
+
   }
 }
